898c02c7b38518b1da881b53aed90e69ad94b725
Andrey <dao.betdevel@gmail.com>
2024-07-25 11:48:09 +0700

    feature: init project, added config

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, UserConfig, AliasOptions } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const alias: AliasOptions = [
+  { find: 'pages', replacement: '/src/pages' },
+  { find: 'app', replacement: '/src/app' },
+  { find: 'entities', replacement: '/src/entities' },
+  { find: 'features', replacement: '/src/features' },
+  { find: 'shared', replacement: '/src/shared' },
+  { find: 'widgets', replacement: '/src/widgets' },
+];
+
+const config: UserConfig = {
+  plugins: [react()],
+  resolve: {
+    alias,
+  },
+};
+
+export default defineConfig(config);
